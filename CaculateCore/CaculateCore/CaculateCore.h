ee70bf5bd50c41c285a7db7937e4a98d1cbf2344
Andy <andy.lin.hswei@gmail.com>
2022-03-02 16:45:51 +0800

    init workspace

---
+++ CaculateCore/CaculateCore/CaculateCore.h
@@ -0,0 +1,18 @@
+//
+//  CaculateCore.h
+//  CaculateCore
+//
+//  Created by Andy on 2022/3/2.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CaculateCore.
+FOUNDATION_EXPORT double CaculateCoreVersionNumber;
+
+//! Project version string for CaculateCore.
+FOUNDATION_EXPORT const unsigned char CaculateCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CaculateCore/PublicHeader.h>
+
+
---
+++ main_2022/main_2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  main_2022
+//
+//  Created by Andy on 2022/3/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
